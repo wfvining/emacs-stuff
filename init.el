fee236c6ec5115ddd014518e5928240540b62f27
Will Vining <wfvining@gmail.com>
2018-08-16 20:38:06 -0600

    moving theme changes out of init.el and into color theme
    Also cleans up the init file by removing things that have been
    commented out for a very long time.

--- init.el
+++ init.el
@@ -57,6 +57,9 @@
 (use-package magit)
 (use-package company)
 (use-package haskell-mode)
+(use-package yaml-mode)
+(use-package cmake-mode)
+(use-package gnuplot-mode)
 
 ;; basic appearance stuff
 (column-number-mode t)
@@ -96,6 +99,9 @@
 ;; and the menu
 (menu-bar-mode -99)
 
+;; enable electric pair mode by default
+(electric-pair-mode 1)
+
 ;; ------ Erlang ------
 ;(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.8.1/emacs"
 ;                      load-path))
@@ -132,121 +138,6 @@
          "* %? %^g\n %T")))
 (add-hook 'org-mode-hook 'turn-on-flyspell)
 
-;(global-set-key "\C-/" 'help-command)
-(global-set-key "\C-h"   'backspace)
-
-;; ------ Rust -----
-;; (add-to-list 'load-path "~/.emacs.d/modes/rust-mode/")
-;; (autoload 'rust-mode "rust-mode" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-;; (custom-set-variables
-;;    '(show-paren-mode t)
-;;    '(size-indication-mode t)
-;;    '(tool-bar-mode nil)
-;;    '(transient-mark-mode t)
-;;    '(column-number-mode t)
-;;    '(indent-tabs-mode nil)
-;;    '(make-backup-files nil))
-
-;; ------ Scheme ------
-(setq scheme-program-name "guile")
-
-;;(require 'mac-key-mode)
-;;(mac-key-mode 1)
-;;(pc-selection-mode 1)
-;;(setq mac-option-modifier 'meta)
-
-;(add-to-list 'load-path "/home/wfv/slime") ;; change this
-
-;(require 'slime)
-;(slime-setup '(slime-repl slime-autodoc slime-fancy-inspector))
-
-;(setq inferior-lisp-program "/usr/local/bin/ccl") ;; change this
-;; use this on a 32Bit Mac (only if your mac is pretty old)
-;;(setq inferior-lisp-program "/usr/local/bin/ccl") ;; if needed, change this
-;(command-execute 'slime)
-
-;; Som variable I might want to set.
-;; (custom-theme-set-variables
-;;  'wfv-gray-nobold
-;;  '(type-break-interval 1200)
-;;  '(electric-pair-mode t)
-;;  '(debug-on-quit t)
-;;  '(which-function-mode t))
-
-;; Ivy configuration
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-
-;; Company mode
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0)
-;; Ivy-based interface to shell and system tools
-;; I'm not planning on using these right now.
-;; (global-set-key (kbd "C-c g") 'counsel-git)
-;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c k") 'counsel-ag)
-;; (global-set-key (kbd "C-x l") 'counsel-locate)
-;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-
-;; Apearance 
-(global-hl-line-mode t) ; turn it on for all modes by default
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (wfv-color)))
- '(custom-safe-themes
-   (quote
-    ("6e64644c0c814c9b987d80078e6d0b2a3cea65d9d2300d80378728e6053842e1" "6ab99af7083324cd2b32f62c52f1f4b2314fb0eaf2157154cfbb11f7827e8f46" "03ca904a51daef657536e604cd2e8679ecf125478fa475710561f56c3c5de103" "7f0d9571d984e14ad672f407b79febf86b6a133e316aba375cfc05860b4142cd" "1651d8d8793a68425206636eb6eddaadcea0a5893e86533456092f37ce4d8cba" "cb4043174063dd4ecef0979403613cbdce8f1a6035404f422f2e480d0524e050" "734f21e68230313dcc77120a853c7fc962c627b41a4656566d09f3ffb0fffa2b" default)))
- '(package-selected-packages
-   (quote
-    (cmake-mode org stack-mode markdown-mode matlab-mode haskell-mode yaml-mode counsel erlang toml-mode company-ghci company-ghc company-erlang company cargo restclient rust-mode magit)))
- '(which-function-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#464646" :foreground "gray70" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "simp" :family "Hack"))))
- '(error ((t (:foreground "firebrick1" :weight bold))))
- '(font-lock-comment-face ((t (:inherit default :foreground "gainsboro" :slant italic))))
- '(fringe ((t (:background "gray35"))))
- '(highlight ((t (:background "gray21"))))
- '(isearch ((t (:background "coral1" :foreground "gray26"))))
- '(link ((t (:foreground "SteelBlue1" :underline t))))
- '(minibuffer-prompt ((t (:foreground "SteelBlue1"))))
- '(mode-line ((t (:background "gray55" :foreground "black" :box (:line-width -1 :style released-button)))))
- '(org-date ((t (:foreground "turquoise" :underline t))))
- '(org-document-info ((t (:foreground "white smoke" :slant italic))))
- '(org-document-title ((t (:foreground "white" :weight bold))))
- '(org-footnote ((t (:inherit link))))
- '(org-list-dt ((t (:foreground "deep sky blue" :weight bold))))
- '(org-table ((t (:foreground "DodgerBlue1"))))
- '(org-verbatim ((t (:inherit nil :foreground "white" :weight semi-bold))))
- '(region ((t (:background "slate grey"))))
- '(shadow ((t (:foreground "grey16"))))
- '(show-paren-match ((t (:background "gold" :foreground "black"))))
- '(show-paren-mismatch ((t (:background "deep pink" :foreground "gray18")))))
-; '(org-verbatim ((t (:foreground "white" :slant normal :family "Mono")))))
-
 ;(require 'org-publish)
 (setq org-publish-project-alist
       '(
@@ -282,21 +173,64 @@
         
         ))
 
- ;; '(default ((t (:inherit nil :stipple nil :background "#464646" :foreground "gray70" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "DAMA" :family "Hack"))))
- ;; '(error ((t (:foreground "firebrick1" :weight bold))))
- ;; '(fringe ((t (:background "gray35"))))
- ;; '(highlight ((t (:background "gray21"))))
- ;; '(isearch ((t (:background "coral1" :foreground "gray26"))))
- ;; '(link ((t (:foreground "SteelBlue1" :underline t))))
- ;; '(minibuffer-prompt ((t (:foreground "SteelBlue1"))))
- ;; '(mode-line ((t (:background "gray55" :foreground "black" :box (:line-width -1 :style released-button)))))
- ;; '(org-date ((t (:foreground "turquoise" :underline t))))
- ;; '(org-document-info ((t (:foreground "white smoke" :slant italic))))
- ;; '(org-document-title ((t (:foreground "white" :weight bold))))
- ;; '(org-footnote ((t (:inherit link))))
- ;; '(org-table ((t (:foreground "DodgerBlue1"))))
- ;; '(region ((t (:background "slate grey"))))
- ;; '(shadow ((t (:foreground "grey16"))))
- ;; '(show-paren-match ((t (:background "gold" :foreground "black"))))
- ;; '(show-paren-mismatch ((t (:background "deep pink" :foreground "gray18")))))
+;; ------ Scheme ------
+(setq scheme-program-name "guile")
 
+;;(require 'mac-key-mode)
+;;(mac-key-mode 1)
+;;(pc-selection-mode 1)
+;;(setq mac-option-modifier 'meta)
+
+;; Ivy configuration
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "C-c l") 'counsel-find-library)
+(global-set-key (kbd "C-c i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "C-c u") 'counsel-unicode-char)
+
+;; Company mode
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0)
+;; Ivy-based interface to shell and system tools
+;; I'm not planning on using these right now.
+;; (global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; (global-set-key (kbd "C-c k") 'counsel-ag)
+;; (global-set-key (kbd "C-x l") 'counsel-locate)
+;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+
+;; Apearance 
+(global-hl-line-mode t) ; turn it on for all modes by default
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (wfv-color)))
+ '(custom-safe-themes
+   (quote
+    ("9d4bffc05c1f5aeb364a141c2bf886ce2b7ae3590cd7d2d4873fd55e759af715" "234a4d07d075898ecc6341519196b2d0741d89c917036b04d9a32f8e76049b91" "6e64644c0c814c9b987d80078e6d0b2a3cea65d9d2300d80378728e6053842e1" "6ab99af7083324cd2b32f62c52f1f4b2314fb0eaf2157154cfbb11f7827e8f46" "03ca904a51daef657536e604cd2e8679ecf125478fa475710561f56c3c5de103" "7f0d9571d984e14ad672f407b79febf86b6a133e316aba375cfc05860b4142cd" "1651d8d8793a68425206636eb6eddaadcea0a5893e86533456092f37ce4d8cba" "cb4043174063dd4ecef0979403613cbdce8f1a6035404f422f2e480d0524e050" "734f21e68230313dcc77120a853c7fc962c627b41a4656566d09f3ffb0fffa2b" default)))
+ '(package-selected-packages
+   (quote
+    (cmake-mode org stack-mode markdown-mode matlab-mode haskell-mode yaml-mode counsel erlang toml-mode company-ghci company-ghc company-erlang company cargo restclient rust-mode magit)))
+ '(which-function-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
